a89f2468f898d48578f036542f680499115b8206
dandean <danwdean@gmail.com>
2010-10-28 22:50:56 -0700

    progress on refactoring for normalized validation array

--- model.js
+++ model.js
@@ -3,8 +3,9 @@
 // TODO: field validators should consist on an Array
 
 function create(modelDefinition) {
-  
-  return (function(def) {
+    
+  // TODO: what the fuck is "connection" and how does it work?
+  return (function(def, connection) {
 
     return function(data) {
       var initialized = false,
@@ -25,88 +26,92 @@ function create(modelDefinition) {
         
         Object.keys(def).forEach(function(key) {
 
+          // Add definition keys as get/set fields on the resulting model object
           Object.defineProperty(this, key, {
             get: function() { return properties[key]; },
             set: function(x) {
               unvalidated = true;
               properties[key] = x;
             },
-            enumerable: true,
-            configurable: true
+            enumerable: true
           });
 
-          var validators = [];
-
-          // Convert `required` flag into a validator
+          var rules = {
+            defaultValue: undefined,
+            validation: []
+          };
           
-          if (def[key].required) {
-            validators.push({
-              test: function(value) {
-                var isValid =  !(value == null || value == undefined || value == "");
-                return isValid;
-              },
-              message: '"{key}" is required but has no value.'
-            });
-
-            delete def[key].required;
-          }
+          var spec = def[key];
           
-          if (def[key].validator) {
-            if (Array.isArray(def[key].validator)) {
-              for(var i=0; i<def[key].validator.length; i++) {
-                validators.push(def[key].validator[i]);
+          if (Array.isArray(spec)) {
+            // spec is an array, use it as an array of validator defs
+            // no defaultValue
+            rules.validation = spec;
+
+          } else {
+            if ("validation" in spec) {
+              if (Array.isArray(spec.validation)) {
+                // spec has validation array, use it as an array of validator defs
+                rules.validation = spec.validation;
+
+                if ("defaultValue" in spec) {
+                  rules.defaultValue = spec.defaultValue;
+                }
+
+              } else if ("test" in spec.validation && "message" in spec.validation) {
+                // spec is a validator def, add it to the validator defs
+                rules.validation.push(spec.validation);
+
+                if ("defaultValue" in spec) {
+                  rules.defaultValue = spec.defaultValue;
+                }
               }
-            } else {
-              validators.push(def[key].validator);
-            }
 
-            delete def[key].validator;
-          }
-          
-          if (validators.length) {
-            def[key].validators = validators;
+            } else if ("test" in spec && "message" in spec) {
+              rules.validation.push(spec);
+            }
           }
           
         }.bind(this));
-
-        Object.defineProperty(this, "valid", {
-          get: function() {
-            
-            if (unvalidated) {
-              errors = [];
-              valid = true;
-              
-              for (var key in def) {
-                var value = properties[key];
-                
-                if (def[key].validators) {
-                  
-                  def[key].validators.forEach(function(v) {
-                    var defaultValue = def[key].defaultValue;
-                    var result = (typeof(defaultValue) != 'undefined' && defaultValue == value) ||  v.test(value);
-                    
-                    if (!result) {
-                      valid = false;
-                      errors.push(v.message.replace("{key}", key));
-                    }
-                  }, this);
-                }
-              }
-              unvalidated = false;
-            }
-            return valid;
-          },
-          enumerable: false,
-          configurable: false
-        });
-
-        Object.defineProperty(this, "errors", {
-          get: function() {
-            return errors;
-          },
-          enumerable: false,
-          configurable: false
-        });
+        
+//        Object.defineProperty(this, "valid", {
+//          get: function() {
+//            
+//            if (unvalidated) {
+//              errors = [];
+//              valid = true;
+//              
+//              for (var key in def) {
+//                var value = properties[key];
+//                
+//                if (def[key].validators) {
+//                  
+//                  def[key].validators.forEach(function(v) {
+//                    var defaultValue = def[key].defaultValue;
+//                    var result = (typeof(defaultValue) != 'undefined' && defaultValue == value) ||  v.test(value);
+//                    
+//                    if (!result) {
+//                      valid = false;
+//                      errors.push(v.message.replace("{key}", key));
+//                    }
+//                  }, this);
+//                }
+//              }
+//              unvalidated = false;
+//            }
+//            return valid;
+//          },
+//          enumerable: false,
+//          configurable: false
+//        });
+//
+//        Object.defineProperty(this, "errors", {
+//          get: function() {
+//            return errors;
+//          },
+//          enumerable: false,
+//          configurable: false
+//        });
       }
     };
 
@@ -114,3 +119,36 @@ function create(modelDefinition) {
 }
 
 exports.create = create;
+
+var Model = {
+  None: {
+    test: function() { return true; },
+    message: "This validator will always pass."
+  },
+  Required: {
+    test: function(x) {
+      return !(value == null || value == undefined || value == "");
+    },
+    message: '"{field}" is required but has no value.'
+  },
+  EmailAddress: {
+    test: function(x) { throw new Error("Not implemented."); },
+    message: "Not implemented"
+  },
+  PhoneUS: {
+    test: function(x) { throw new Error("Not implemented."); },
+    message: "Not implemented"
+  },
+  Url: {
+    test: function(x) { throw new Error("Not implemented."); },
+    message: "Not implemented"
+  }
+};
+
+Object.keys(Model).forEach(function(m) {
+  Object.defineProperty(exports, m, {
+    enumerable: true,
+    configurable: false,
+    value: Model[m]
+  });
+});
--- unit-tests.js
+++ unit-tests.js
@@ -1,80 +1,136 @@
 var Assert = require("assert"),
     Model = require("./model");
 
-
-// TWO-PROPERTIES, NO VALIDATORS
-var ModelHasNameAndPhone = Model.create({
-  name: {},
-  phone: {}
-});
-
-var modelShouldHaveNameAndPhone = new ModelHasNameAndPhone();
-Assert.ok(modelShouldHaveNameAndPhone.valid, "Instance has no validators, should be valid.");
-
-var keys = Object.keys(modelShouldHaveNameAndPhone);
-Assert.equal(keys.length, 2, "Instance should have 2 keys.");
-Assert.ok(keys.indexOf("name") > -1, "Instance should have a 'name' key.");
-Assert.ok(keys.indexOf("phone") > -1, "Instance should have a 'phone' key.");
-
-
-// REQUIRED FIELD flag
-var NameIsRequired = Model.create({
-  name: {
-    required: true
-  }
-});
-
-var nameShouldBeRequired = new NameIsRequired();
-Assert.ok(nameShouldBeRequired.valid === false, "Instance instance should be invalid");
-Assert.equal(nameShouldBeRequired.errors.length, 1, "Instance should have 1 error.");
-
-nameShouldBeRequired.name = "name";
-Assert.ok(nameShouldBeRequired.valid);
-Assert.equal(nameShouldBeRequired.errors.length, 0, "Instance should have 0 errors.");
-
-
-// REGEXP VALIDATORS SHOULD AUTO-EXPAND INTO TEST METHODS
-var HasRegExpValidator = Model.create({
-  numbers: {
-    validator: {
-      test: /^\d+$/,
-      message: "The '{field}' field should only consist of numbers"
+// -> EXAMPLE MODEL DEFINITION, AND ALL OF ITS VARIANTS.
+// AT THIS POINT, THE CURRENT ITERATION WOULD BE A SINGLE FIELD SPEC
+var X = Model.create({
+  prop1: Model.None,
+  prop2: Model.Required,
+  prop3: [Model.Required, Model.Url],
+  prop4: {
+    defaultValue: "Enter a URL",
+    validation: Model.None
+  },
+  prop5: {
+    defaultValue: "Enter a URL",
+    validation: Model.Required
+  },
+  prop6: {
+    defaultValue: "Enter a URL",
+    validation: [Model.Required, Model.Url]
+  },
+  prop7: {
+    defaultValue: "Enter a URL",
+    validation: [Model.Url, {
+      test: function(x) {
+        return !(value == null || value == undefined || value == "");
+      },
+      message: '"{field}" is required but has no value.'
+    }]
+  },
+  prop8: {
+    defaultValue: "Enter a URL",
+    validation: {
+      test: function(x) {
+        return !(value == null || value == undefined || value == "");
+      },
+      message: '"{field}" is required but has no value.'
     }
   }
 });
 
-var hasRegExpValidatorWhichShouldSucceed = new HasRegExpValidator({
-  numbers: 500
-});
-Assert.ok(hasRegExpValidatorWhichShouldSucceed.valid, "Should be valid.");
 
-var hasRegExpValidatorWhichShouldFail = new HasRegExpValidator({
-  numbers: "hi there"
-});
-Assert.ok(hasRegExpValidatorWhichShouldFail.valid === false, "Should NOT be valid.");
+var x = new X();
 
 
-// DEFAULT VALUES AREN'T PASSED THROUGH VALIDATION
-var HasDefaultValueWhichDoesNotMatchValidator = Model.create({
-  numbers: {
-    defaultValue: "This is not a number",
-    validator: {
-      test: /^\d+$/,
-      message: "The '{field}' field should only consist of numbers"
-    }
-  }
-});
+//// TWO-PROPERTIES, NO VALIDATORS
+//var ModelHasNameAndPhone = Model.create({
+//  name: {},
+//  phone: {}
+//});
+//
+//var modelShouldHaveNameAndPhone = new ModelHasNameAndPhone();
+//Assert.ok(modelShouldHaveNameAndPhone.valid, "Instance has no validators, should be valid.");
+//
+//var keys = Object.keys(modelShouldHaveNameAndPhone);
+//Assert.equal(keys.length, 2, "Instance should have 2 keys.");
+//Assert.ok(keys.indexOf("name") > -1, "Instance should have a 'name' key.");
+//Assert.ok(keys.indexOf("phone") > -1, "Instance should have a 'phone' key.");
+//
+//
+//// REQUIRED FIELD flag
+//var NameIsRequired = Model.create({
+//  name: {
+//    required: true
+//  }
+//});
+//
+//var nameShouldBeRequired = new NameIsRequired();
+//Assert.ok(nameShouldBeRequired.valid === false, "Instance instance should be invalid");
+//Assert.equal(nameShouldBeRequired.errors.length, 1, "Instance should have 1 error.");
+//
+//nameShouldBeRequired.name = "name";
+//Assert.ok(nameShouldBeRequired.valid);
+//Assert.equal(nameShouldBeRequired.errors.length, 0, "Instance should have 0 errors.");
+//
+//
+//// REGEXP VALIDATORS SHOULD AUTO-EXPAND INTO TEST METHODS
+//var HasRegExpValidator = Model.create({
+//  numbers: {
+//    validator: {
+//      test: /^\d+$/,
+//      message: "The '{field}' field should only consist of numbers"
+//    }
+//  }
+//});
+//
+//var hasRegExpValidatorWhichShouldSucceed = new HasRegExpValidator({
+//  numbers: 500
+//});
+//Assert.ok(hasRegExpValidatorWhichShouldSucceed.valid, "Should be valid.");
+//
+//var hasRegExpValidatorWhichShouldFail = new HasRegExpValidator({
+//  numbers: "hi there"
+//});
+//Assert.ok(hasRegExpValidatorWhichShouldFail.valid === false, "Should NOT be valid.");
+//
+//
+//// DEFAULT VALUES AREN'T PASSED THROUGH VALIDATION
+//var HasDefaultValueWhichDoesNotMatchValidator = Model.create({
+//  numbers: {
+//    defaultValue: "This is not a number",
+//    validator: {
+//      test: /^\d+$/,
+//      message: "The '{field}' field should only consist of numbers"
+//    }
+//  }
+//});
+//
+//var hasDefaultValueWhichDoesNotMatchValidator = new HasDefaultValueWhichDoesNotMatchValidator({
+//  numbers: "This is not a number"
+//});
+//Assert.ok(hasDefaultValueWhichDoesNotMatchValidator.valid, "Default value should pass even when they don't match validators.");
+//
+//hasDefaultValueWhichDoesNotMatchValidator.numbers = 500;
+//Assert.ok(hasDefaultValueWhichDoesNotMatchValidator.valid, "New non-default value should pass validator.");
+//
+//hasDefaultValueWhichDoesNotMatchValidator.numbers = "!!!";
+//Assert.ok(hasDefaultValueWhichDoesNotMatchValidator.valid === false, "New non-default value should fail validator.");
 
-var hasDefaultValueWhichDoesNotMatchValidator = new HasDefaultValueWhichDoesNotMatchValidator({
-  numbers: "This is not a number"
-});
-Assert.ok(hasDefaultValueWhichDoesNotMatchValidator.valid, "Default value should pass even when they don't match validators.");
 
-hasDefaultValueWhichDoesNotMatchValidator.numbers = 500;
-Assert.ok(hasDefaultValueWhichDoesNotMatchValidator.valid, "New non-default value should pass validator.");
 
-hasDefaultValueWhichDoesNotMatchValidator.numbers = "!!!";
-Assert.ok(hasDefaultValueWhichDoesNotMatchValidator.valid === false, "New non-default value should fail validator.");
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //var Person = Model.create({
